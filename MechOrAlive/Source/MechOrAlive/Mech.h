d7639070ff29011c71818571443de753fb1e8b5f
Måns Isaksson <mans.95.isaksson@hotmail.com>
2016-06-13 13:20:22 +0200

    uhmmm

---
+++ MechOrAlive/Source/MechOrAlive/Mech.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "Mech.generated.h"
+
+UCLASS()
+class MECHORALIVE_API AMech : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMech();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	
+};
