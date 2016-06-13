a69276ce2d9555c1d6822cf9e367c9360cb37313
Måns Isaksson <mans.95.isaksson@hotmail.com>
2016-06-13 12:34:56 +0200

    En massa projektfiler

---
+++ MechOrAlive/Source/MechOrAlive/MechOrAliveGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MechOrAliveGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHORALIVE_API AMechOrAliveGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
