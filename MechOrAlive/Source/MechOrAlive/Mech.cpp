e4cdab7a51ca36aeeb026ac5ccb7139f3b0e7678
Måns Isaksson <mans.95.isaksson@hotmail.com>
2016-06-13 16:32:22 +0200

    Cool shit

--- MechOrAlive/Source/MechOrAlive/Mech.cpp
+++ MechOrAlive/Source/MechOrAlive/Mech.cpp
@@ -16,12 +16,18 @@ void AMech::BeginPlay()
 void AMech::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void AMech::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 	
+	InputComponent->BindAxis("MoveSideways", this, &AMech::MoveSideways);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &AMech::Jump);
 }
 
+void AMech::MoveSideways(float Value)
+{
+	if (Value != 0.0f)
+		AddMovementInput(FVector(1.f, 0.f, 0.f), Value);
+}
